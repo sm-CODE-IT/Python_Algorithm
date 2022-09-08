7d6d1076c5989114b676c9ce62a29455c24db1a3
soyoung2022 <yuns8671@gmail.com>
2022-09-08 18:59:28 +0900

    Initial Commit

---
+++ 남유민/turtle001.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+t.shape("circle")
+
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
